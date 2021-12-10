1f29faae9c224602034693ca12abd8a1319837cf
add-flav1s <aflavia17_@hotmail.com>
2021-12-09 21:21:15 -0300

    creating the CRUD code

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const animes = ['Kimetsu no Yaiba', 'Naruto', 'Bleach', 'One Piece', 'Dragon Ball Z', 'Attack on Titan', 'Fullmetal Alchemist', 'One Punch'];
+
+
+//retorna 1 anime
+server.get('/animes/:index', (req, res) => {
+    const {
+        index
+    } = req.params;
+
+    return res.json(animes[index]);
+});
+
+//retorna todos os animes
+server.get('/animes', (req, res) => {
+    return res.json(animes);
+})
+
+//criar um novo anime
+server.post('/animes', (req, res) => {
+    const {
+        name
+    } = req.body;
+    animes.push(name);
+
+    return res.json(animes);
+})
+
+
+//atualizar um anime
+server.put('/animes/:index', (req, res) => {
+    const {
+        index
+    } = req.params;
+    const {
+        name
+    } = req.body;
+
+    animes[index] = name;
+    return res.json(animes);
+})
+
+
+//deletar um anime
+server.delete('/animes/:index', (req, res) => {
+    const {
+        index
+    } = req.params;
+
+    animes.splice(index, 1);
+
+    return res.json({
+        message: "o anime foi deletado"
+    })
+})
+
+server.listen(8082);
